c330bd3b03d43c62b9980f153677be2a7793fd56
Stefan Lau <github@stefanlau.com>
2017-11-16 21:03:22 +0100

    Extract parse_args test

--- rust/src/cli.rs
+++ rust/src/cli.rs
@@ -168,3 +168,143 @@ pub fn parse_args(engine_options: &mut EngineOptions, args: Vec<String>) -> Opti
         Err(f) => Some(f.to_string())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    extern crate tempdir;
+    use fs;
+
+    #[test]
+    fn parse_args_should_abort_on_unknown_arguments() {
+        let mut engine_options: super::EngineOptions = Default::default();
+        let input = vec!(String::from("ja2"), String::from("testunknown"));
+        assert_eq!(super::parse_args(&mut engine_options, input).unwrap(), "Unknown arguments: 'testunknown'.");
+    }
+
+    #[test]
+    fn parse_args_should_abort_on_unknown_switch() {
+        let mut engine_options: super::EngineOptions = Default::default();
+        let input = vec!(String::from("ja2"), String::from("--testunknown"));
+        assert_eq!(super::parse_args(&mut engine_options, input).unwrap(), "Unrecognized option: 'testunknown'");
+    }
+
+    #[test]
+    fn parse_args_should_have_correct_fullscreen_default_value() {
+        let mut engine_options: super::EngineOptions = Default::default();
+        let input = vec!(String::from("ja2"));
+        assert_eq!(super::parse_args(&mut engine_options, input), None);
+        assert!(!engine_options.start_in_fullscreen);
+    }
+
+    #[test]
+    fn parse_args_should_be_able_to_change_fullscreen_value() {
+        let mut engine_options: super::EngineOptions = Default::default();
+        let input = vec!(String::from("ja2"), String::from("-fullscreen"));
+        assert_eq!(super::parse_args(&mut engine_options, input), None);
+        assert!(engine_options.start_in_fullscreen);
+    }
+
+    #[test]
+    fn parse_args_should_be_able_to_show_help() {
+        let mut engine_options: super::EngineOptions = Default::default();
+        let input = vec!(String::from("ja2"), String::from("-help"));
+        assert_eq!(super::parse_args(&mut engine_options, input), None);
+        assert!(engine_options.show_help);
+    }
+
+    #[test]
+    fn parse_args_should_continue_with_multiple_known_switches() {
+        let mut engine_options: super::EngineOptions = Default::default();
+        let input = vec!(String::from("ja2"), String::from("-debug"), String::from("-mod"), String::from("a"), String::from("--mod"), String::from("ö"));
+        assert_eq!(super::parse_args(&mut engine_options, input), None);
+        assert!(engine_options.start_in_debug_mode);
+        assert_eq!(engine_options.mods, vec!["a", "ö"]);
+    }
+
+    #[test]
+    fn parse_args_should_fail_with_unknown_resversion() {
+        let mut engine_options: super::EngineOptions = Default::default();
+        let input = vec!(String::from("ja2"), String::from("--resversion"), String::from("TESTUNKNOWN"));
+        assert_eq!(super::parse_args(&mut engine_options, input).unwrap(), "Resource version TESTUNKNOWN is unknown");
+    }
+
+    #[test]
+    fn parse_args_should_return_the_correct_resversion_for_russian() {
+        let mut engine_options: super::EngineOptions = Default::default();
+        let input = vec!(String::from("ja2"), String::from("-resversion"), String::from("RUSSIAN"));
+        assert_eq!(super::parse_args(&mut engine_options, input), None);
+        assert_eq!(engine_options.resource_version, super::ResourceVersion::RUSSIAN);
+    }
+
+    #[test]
+    fn parse_args_should_return_the_correct_resversion_for_italian() {
+        let mut engine_options: super::EngineOptions = Default::default();
+        let input = vec!(String::from("ja2"), String::from("-resversion"), String::from("ITALIAN"));
+        assert_eq!(super::parse_args(&mut engine_options, input), None);
+        assert_eq!(engine_options.resource_version, super::ResourceVersion::ITALIAN);
+    }
+
+    #[test]
+    fn parse_args_should_return_the_correct_resolution() {
+        let mut engine_options: super::EngineOptions = Default::default();
+        let input = vec!(String::from("ja2"), String::from("--res"), String::from("1120x960"));
+        assert_eq!(super::parse_args(&mut engine_options, input), None);
+        assert_eq!(engine_options.resolution, (1120, 960));
+    }
+
+    #[test]
+    #[cfg(target_os = "macos")]
+    fn parse_args_should_return_the_correct_canonical_data_dir_on_mac() {
+        let mut engine_options: super::EngineOptions = Default::default();
+        let temp_dir = tempdir::TempDir::new("ja2-tests").unwrap();
+        let dir_path = temp_dir.path().join("foo");
+
+        fs::create_dir_all(dir_path).unwrap();
+
+        let input = vec!(String::from("ja2"), String::from("--datadir"), String::from(temp_dir.path().join("foo/../foo/../").to_str().unwrap()));
+
+        assert_eq!(super::parse_args(&mut engine_options, input), None);
+
+        let temp = fs::canonicalize(temp_dir.path()).expect("Problem during building of reference value.");
+
+        assert_eq!(engine_options.vanilla_data_dir, temp);
+    }
+
+    #[test]
+    #[cfg(all(not(windows), not(target_os = "macos")))]
+    fn parse_args_should_return_the_correct_canonical_data_dir_on_linux() {
+        let mut engine_options: super::EngineOptions = Default::default();
+        let temp_dir = tempdir::TempDir::new("ja2-tests").unwrap();
+        let dir_path = temp_dir.path().join("foo");
+
+        fs::create_dir_all(dir_path).unwrap();
+
+        let input = vec!(String::from("ja2"), String::from("--datadir"), String::from(temp_dir.path().join("foo/../foo/../").to_str().unwrap()));
+
+        assert_eq!(super::parse_args(&mut engine_options, input), None);
+        assert_eq!(engine_options.vanilla_data_dir, temp_dir.path());
+    }
+
+    #[test]
+    #[cfg(windows)]
+    fn parse_args_should_return_the_correct_canonical_data_dir_on_windows() {
+        let mut engine_options: super::EngineOptions = Default::default();
+        let temp_dir = tempdir::TempDir::new("ja2-tests").unwrap();
+        let dir_path = temp_dir.path().join("foo");
+
+        fs::create_dir_all(dir_path).unwrap();
+
+        let input = vec!(String::from("ja2"), String::from("--datadir"), String::from(temp_dir.path().to_str().unwrap()));
+
+        assert_eq!(super::parse_args(&mut engine_options, input), None);
+        assert_eq!(engine_options.vanilla_data_dir, temp_dir.path());
+    }
+
+    #[test]
+    fn parse_args_should_fail_with_non_existing_directory() {
+        let mut engine_options: super::EngineOptions = Default::default();
+        let input = vec!(String::from("ja2"), String::from("--datadir"), String::from("somethingelse"));
+
+        assert_eq!(super::parse_args(&mut engine_options, input), Some(String::from("Please specify an existing datadir.")));
+    }
+}
